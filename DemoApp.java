49041d91bdc442fe39f60ff3cfdb490321739fb6
phil-lewis-exe <p.lewis2@exeter.ac.uk>
2025-03-19 10:51:37 +0000

    better demo

--- DemoApp.java
+++ DemoApp.java
@@ -2,12 +2,22 @@ import java.time.LocalDate;
 
 public class DemoApp {
     public static void main(String[] args) {
-        System.out.println("The date today is: " + DateProvider.now());
-        DateProvider.setOffset(1);
-        System.out.println("The date tomorrow is: " + DateProvider.now());
-        DateProvider.addDay();
-        System.out.println("The date the day after tomorrow is: " + DateProvider.now());
+        System.out.println("This is a demo of the DateProvider class");
+        System.out.println("Use it by replacing LocalDate.now() with DateProvider.now()\n");
 
-        System.out.println("LocalDate still says the day is: " + LocalDate.now());
+        System.out.println("Default behaviour");
+        System.out.println("Result of DateProvider.now(): " + DateProvider.now());
+        System.out.println("Result of LocalDate.now(): " + LocalDate.now());
+
+        System.out.println("\nCalling DateProvider.addDay()");     
+        DateProvider.addDay();
+        System.out.println("Result of DateProvider.now(): " + DateProvider.now());
+        System.out.println("Result of LocalDate.now(): " + LocalDate.now());
+        
+        System.out.println("\nCalling DateProvider.setOffset(7)");     
+        DateProvider.setOffset(7);
+        System.out.println("Result of DateProvider.now(): " + DateProvider.now());
+        System.out.println("Result of LocalDate.now(): " + LocalDate.now());
+
     }
 }
